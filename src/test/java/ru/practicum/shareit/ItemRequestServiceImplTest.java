6b65a497cb7c17129d46525e0b662edc5596582b
Ekuznecov <e.u.kuznetsov@ya.ru>
2023-05-19 19:35:11 +0300

    feat: ItemRequestServiceImplTest wip

--- src/test/java/ru/practicum/shareit/ItemRequestServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/ItemRequestServiceImplTest.java
@@ -1,4 +1,25 @@
 package ru.practicum.shareit;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.practicum.shareit.item.storage.ItemStorage;
+import ru.practicum.shareit.request.service.ItemRequestServiceImpl;
+import ru.practicum.shareit.request.storage.ItemRequestStorage;
+import ru.practicum.shareit.user.service.UserService;
+import ru.practicum.shareit.user.storage.UserStorage;
+
+@ExtendWith(MockitoExtension.class)
 public class ItemRequestServiceImplTest {
+    @Mock
+    UserService userService;
+    @Mock
+    UserStorage userStorage;
+    @Mock
+    ItemStorage itemStorage;
+    @Mock
+    ItemRequestStorage requestStorage;
+    @InjectMocks
+    ItemRequestServiceImpl requestService;
 }
